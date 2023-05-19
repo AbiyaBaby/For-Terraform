04bba68fa3e699db6246daa4f66ce4e49215c3ab
AbiyaBaby <127095322+AbiyaBaby@users.noreply.github.com>
2023-05-19 15:03:21 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,8 @@ terraform {
   required_version = "~> 1.3.1"
   required_providers {
     newrelic = {
-      source = "newrelic/newrelic"  
+      source = "newrelic/newrelic" 
+      version = "~> 3.23"
     }
   }
 }
